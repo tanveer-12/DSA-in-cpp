c498767516dc190b52173c959ec570bfd76be0d4
Tanveer Kaur <tanveerkaur1292@outlook.com>
2025-10-12 11:03:35 -0400

    LinkedList started

---
+++ LinkedList/intro.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int data)
+    : data{data},
+    next{nullptr}
+    {}
+
+    Node(int data, Node* next)
+    : data{data},
+    next{next}
+    {}
+};
+
+int main(){
+    std::vector<int> arr = {2,4,5,6};
+    Node x = Node(arr[1],nullptr);
+    Node *y = &x;
+
+    // or 
+    Node* z = new Node(arr[1], nullptr);
+    std::cout<<y->data<<std::endl;
+    std::cout<<y->next;
+    std::cout<<z->data<<std::endl;
+    std::cout<<z->next;
+}
